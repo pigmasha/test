fc560d4785f5eabc44967d2cd3cacb21a14cf898
mkachalova <masha@lsi.ru>
2019-07-01 08:53:33 +0300

    E7: fix

--- E7/Source/Step_8_shift_enum.swift
+++ E7/Source/Step_8_shift_enum.swift
@@ -106,6 +106,7 @@ struct Step_8_shift_enum {
         case 7: isGood = hh.nonZeroCount == 1
         case 8: isGood = hh.nonZeroCount == 3*s
         case 9: isGood = hh.nonZeroCount == 7*s && hh.rows[0][0].isZero && !hh.rows[0][s].isZero
+        case 11: isGood = hh.nonZeroCount == 3 && !hh.rows[0][2*s].isZero
         default: break
         }
         guard isGood else {
